71421177a6cc160067c87f3e4bea8ce435888f58
prisa1987 <air_pd2@hotmail.co.th>
2015-05-13 23:22:33 +0700

    Refine code

--- app/src/main/kotlin/com/showtime/prisa/showtimedemo/Presentation/Model/URLConnection.kt
+++ app/src/main/kotlin/com/showtime/prisa/showtimedemo/Presentation/Model/URLConnection.kt
@@ -1,6 +1,9 @@
 package com.showtime.prisa.showtimedemo.Presentation.Model
 
 import android.util.Log
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import com.loopj.android.http.RequestParams
 import org.apache.http.HttpResponse
 import org.apache.http.client.HttpClient
 import org.apache.http.client.ResponseHandler
@@ -8,21 +11,25 @@ import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.BasicResponseHandler
 import org.apache.http.impl.client.DefaultHttpClient
 import java.net.URI
+import kotlin.properties.Delegates
 
 /**
  * Connect to MovieDB
  */
 public class URLConnection {
 
+
+
     //Creating HTTP Client and HTTP Get
     val client: HttpClient = DefaultHttpClient()
     val httpGet: HttpGet = HttpGet("")
 
     //Set URI
-    fun setURI(url:String){
+    fun setURI(url:String) {
         var uri: URI = URI(url)
         httpGet.setURI(uri)
     }
+
 
     //Making HTTP Request
     fun getResponseBody(): String {
