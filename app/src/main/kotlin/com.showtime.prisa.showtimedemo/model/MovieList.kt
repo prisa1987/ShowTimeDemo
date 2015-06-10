591ee70cbed278ea92fc442d7f2978fe99fed94c
Prisa <prisa1987@gmail.com>
2015-06-10 23:36:33 +0700

    Update code follow the suggestion

--- app/src/main/kotlin/com.showtime.prisa.showtimedemo/model/MovieList.kt
+++ app/src/main/kotlin/com.showtime.prisa.showtimedemo/model/MovieList.kt
@@ -4,5 +4,5 @@ package com.showtime.prisa.showtimedemo.model
  * Created by Admin on 5/29/15.
  */
 public class MovieList {
-    public var results:List<Movie>? = null
+    public var results: List<Movie>? = null
 }
